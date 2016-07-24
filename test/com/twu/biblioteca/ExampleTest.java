76f0ef74804e5a2607feb86e28aa298498cfc12b
SigridZhangSY <sigridzhang0603@163.com>
2016-07-24 22:20:52 +0800

    show welcome message

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,41 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
+
+    PrintStream console = null;
+    ByteArrayOutputStream bytes = null;
+    BibliotecaApp bibliotecaApp = new BibliotecaApp();
+    String[] args = new String[0];
+
 
     @Test
     public void test() {
         assertEquals(1, 1);
     }
+
+    @Test
+    public void WelcomeMessageTest(){
+        bytes = new ByteArrayOutputStream();
+        console = System.out;
+        System.setOut(new PrintStream(bytes));
+        bibliotecaApp.main(args);
+        String s = new String("Welcome, Biblioteca is available!\n");
+
+        assertEquals(s, bytes.toString());
+
+    }
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(console);
+    }
+
 }
